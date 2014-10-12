c990a7dc4ae88768ef290409f86761ae98d45c57
Te Vallee <astronomemusic@gmail.com>
2014-10-11 16:40:19 -1000

    initial commit

---
+++ Runner.js
@@ -0,0 +1,5 @@
+// new file named runner.js
+var _ = require('./waterpolodash'); // your "whatever" file name (no .js is needed)
+ 
+var result = _.test();
+console.log(result);
---
+++ waterpolodash.js
@@ -0,0 +1,12 @@
+// create your project file to "whatever.js" for example cholo.js
+// put this code in here!
+ 
+module.exports = {
+  // compact: function(){},
+  // difference: function(){},
+  //etc...
+  
+
+  test: function(){console.log("test! rename this file to your project name")}
+};
+ 
