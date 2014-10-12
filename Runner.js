94898e47636e84f3790489c5b20a9ffd5cc051f3
Te Vallee <astronomemusic@gmail.com>
2014-10-11 20:04:36 -1000

    some of the array section

--- Runner.js
+++ Runner.js
@@ -1,5 +1,15 @@
 // new file named runner.js
 var _ = require('./waterpolodash'); // your "whatever" file name (no .js is needed)
- 
-var result = _.test();
-console.log(result);
+
+// not done yet 
+var compact_result = _.compact([0,1,2,'',false,3]);
+console.log(compact_result);
+
+var initial_result = _.initial([1,2,3]);
+console.log(initial_result);
+
+var last_result = _.last([1,2,3,6,7,'meow']);
+console.log(last_result);
+
+var rest_result = _.rest([1,2,3]);
+console.log(rest_result);
--- waterpolodash.js
+++ waterpolodash.js
@@ -6,7 +6,48 @@ module.exports = {
   // difference: function(){},
   //etc...
   
+  // ARRAYS
+  // _.compact: creates an array with all falsey values removed. The values false, null, 0, "", undefined, and NaN are all falsey.
+  compact: function(array){
+    for(var i =0; i<array.length; i++){
+      if((array[i] === 0) || (array[i] === false) || (array[i] === '') || (array[i] === undefined) || (array[i] === NaN)){
+        array.splice(i);
+        return array;
+      } 
+    }
+  },
+  //_.difference: Creates an array excluding all values of the provided arrays using strict equality for comparisons, i.e. ===.
+  difference: function(){
 
-  test: function(){console.log("test! rename this file to your project name")}
-};
+  },
+  // _.findIndex: This method is like _.find except that it returns the index of the first element that passes the callback check, instead of the element itself.
+  findIndex: function(){
+
+  },
+  // _.findLastIndex: this method is like _.findIndex except that it iterates over elements of a collection from right to left.
+  findLastIndex: function(){
+
+  },
+  // _.indexOf: Gets the index at which the first occurrence of value is found using strict equality for comparisons, i.e. ===. If the array is already sorted providing true for fromIndex will run a faster binary search.
+  indexOf: function(){
+
+  },
+   // _.initial: Gets all but the last element or last n elements of an array.
+    initial: function(array){
+     array.pop();
+     return array;
+   },
+  // _.last: gets the last element or n elements of an array.
+  last: function(array){
+    return array[array.length-1];
+  },
+  // _.rest: gets all but the first element or first elements of an array.
+  rest: function(array){
+    array.shift();
+    return array;
+  },
+
+}
+
+
  
